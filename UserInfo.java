7132168ed2f1053498a887a32dfd05df3f31222e
Buse Ayrancıoğlu <122692435+buayrnx@users.noreply.github.com>
2024-02-07 11:59:11 +0300

    Update and rename Main.java to UserInfo.java

--- UserInfo.java
+++ UserInfo.java
@@ -1,6 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class UserInfo {
     public static void main(String[] args) {
         int newYear = 2024;
         String name = "Buse";
