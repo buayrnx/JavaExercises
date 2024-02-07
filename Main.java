2cbe986aa2eaa254947a6d0c8908c7d138b165fb
Buse Ayrancıoğlu <122692435+buayrnx@users.noreply.github.com>
2024-02-07 11:54:30 +0300

    Add files via upload
    Beginner Java codes

---
+++ Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int newYear = 2024;
+        String name = "Buse";
+        int birthYear = 2004;
+        double salary = 50000.0;
+        char gender = 'F';
+        boolean isStudent = true;
+
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + (newYear - birthYear));
+        System.out.println("Salary:" + salary);
+        System.out.println("Gender:" + gender);
+        System.out.println("Is Student:" + isStudent);
+    }
+}
